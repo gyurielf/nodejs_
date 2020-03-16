a5b085c0062d952a8e7625a5ec8fde7f8713869d
gyurielf <mail.gyorgy.kallai@gmail.com>
2020-03-16 20:22:45 +0100

    npm packages - prettier

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////
@@ -14,7 +14,6 @@ console.log(textIn);
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
 fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
 console.log('File written!'); */
-
 
 // Non-blocking synchronous way
 /* fs.readFile('./txt/starta.txt', 'utf-8', (err, data1) => {
@@ -37,64 +36,80 @@ console.log('Will read file!'); */
 /////////////////////////////
 /// SERVER
 
-const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const templateCards = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const templateCards = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url);
-    const {query, pathname} = url.parse(req.url, true);
-    // const pathName = req.url;
+/* A MAP egy tömb összes elemét átalakítja egy másik tömbbé! */
+const slugs = dataObj.map(el =>
+  slugify(`${el.productName} ${el.id}`, { lower: true })
+);
 
-    // HOME PAGE
-    if (pathname === '/') {
-        res.end('HOME');
-        
-        // OVERVIEW PAGE
-    } else if (pathname === '/overview') {
-        res.writeHead(200, {
-            'Content-type': 'text/html; charset=UTF-8'
-        });
-        /* Egy tömbbön loop-al végigmegyek. Az első el, a tömb aktuális eleme. Egy functiont (replaceTemplate) ráhívok minden elemre, 
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathName = req.url;
+
+  // HOME PAGE
+  if (pathname === '/') {
+    res.end('HOME');
+
+    // OVERVIEW PAGE
+  } else if (pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html; charset=UTF-8'
+    });
+    /* Egy tömbbön loop-al végigmegyek. Az első el, a tömb aktuális eleme. Egy functiont (replaceTemplate) ráhívok minden elemre, 
         ahol a functionnak két argumentet adok be, a templateCards-ot (egy html oldal, a benne lévő összes tartalommal) 
         és az aktuális elemét a dataObj tömbbnek.
         Végezetül, hogy ne egy tömbböt kapjunk a cardsHtml változóba, egy nagy string-et csinálunk belőle.
         ezt úgy tudjuk megtenni, hogy a tömb végén .join('')-t használunk ez az összes ide tartozó elemet joinolni foja egy stringbe.
          */
-        const cardsHtml = dataObj.map( el => replaceTemplate(templateCards, el)).join('');
+    const cardsHtml = dataObj
+      .map(el => replaceTemplate(templateCards, el))
+      .join('');
 
-        /* ITT a templateOverview (itt most egy html file) replacelem a {%PRODUCT_CARDS%}-ot (ez a productok helye, ahová ki kel lrajzolni a productokat.) */
-        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    /* ITT a templateOverview (itt most egy html file) replacelem a {%PRODUCT_CARDS%}-ot (ez a productok helye, ahová ki kel lrajzolni a productokat.) */
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-        res.end(output);
+    res.end(output);
 
-        // PRODUCT PAGE
-    } else if (pathname === '/product')  {
-        res.writeHead(200, {'Content-type': 'text/html; charset=UTF-8'});
-        const product = dataObj[query.id];
-        const output = replaceTemplate(templateProduct, product);
-        // console.log(query);        
-        res.end(output);
+    // PRODUCT PAGE
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html; charset=UTF-8' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    // console.log(query);
+    res.end(output);
 
-        // API
-    } else if (pathname === '/api'){
-        res.writeHead(200, {'Content-type': 'application/json'});       
-        res.end(data);       
-        
-        // NOT FOUND
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html; charset=UTF-8'
-        });
-        res.end('<h1>Page not found!</h1>');
-    }
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
 
+    // NOT FOUND
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html; charset=UTF-8'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
+
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to request on port 8000');
+  console.log('Listening to request on port 8000');
 });
-
- 
