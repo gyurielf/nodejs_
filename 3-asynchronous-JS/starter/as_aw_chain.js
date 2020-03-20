c6b9ae01b7a1ba082dfc6c46d88a8c1904542be6
gyurielf <mail.gyorgy.kallai@gmail.com>
2020-03-20 22:42:33 +0100

    AS/AW - Last lecture - DONE

--- 3-asynchronous-JS/starter/as_aw_chain.js
+++ 3-asynchronous-JS/starter/as_aw_chain.js
@@ -27,23 +27,12 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res1Pro = superagent.get(
+    const res = await superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    const res2Pro = superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`
-    );
-    const res3Pro = superagent.get(
-      `https://dog.ceo/api/breed/${data}/images/random`
-    );
+    console.log(res.body.message);
 
-    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
-    const imgs = all.map(el => el.body.message);
-    console.log(imgs);
-
-    // console.log(res.body.message);
-
-    await writeFilePro('dog-img.txt', imgs.join('\r\n'));
+    await writeFilePro('dog-img.txt', res.body.message);
     console.log('Random dog image saved to file!');
   } catch (err) {
     err.message ? console.log(err.message) : console.log(err);
@@ -57,7 +46,7 @@ const getDogPic = async () => {
     console.log('1: will get dog pics!');
     const x = await getDogPic();
     console.log(x);
-    console.log('3: Done geting dog pics!');
+    console.log('3: Done geting dog pics!'); 
   } catch (err) {
     console.log('Error!');
   }
