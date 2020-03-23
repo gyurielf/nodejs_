26d32049cfef91a5c1fe9fadcfcbca5b8aedfe41
gyurielf <mail.gyorgy.kallai@gmail.com>
2020-03-23 09:54:27 +0100

    Middlewares and Morgan logging / DONE

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.disable('x-powered-by');
-app.use(express.json());
 
+
+// ###### 1) MIDLEWARES
 // If we dont use the next at the and, the req res cycle will be stuck.
+app.use(morgan('dev'));
+app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
   next();
@@ -26,7 +30,7 @@ const tours = JSON.parse(
     }
   )
 );
-
+// ###### ROUTE HANDLERS
 // READ/LIST ALL TOURS
 const getAllTours = (req, res) => {
   console.log(req.requestTime)
@@ -113,7 +117,7 @@ const deleteTour = (req, res) => {
   });
 };
 
-// ROUTES/ENDPOINTS
+// ###### 3) ROUTES/ENDPOINTS
 // app.get(`/api/v1/tours`, getAllTours);
 // app.get(`/api/v1/tours/:id`, getTour);
 // app.post(`/api/v1/tours`, createTour);
@@ -132,6 +136,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// ###### 4) START SERVER
 // SERVER
 const port = 8000;
 app.listen(port, () => {
