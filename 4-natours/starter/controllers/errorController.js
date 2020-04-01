ac8a35195731e9bf4ca0e4648f1feeb8c7c13859
gyurielf <mail.gyorgy.kallai@gmail.com>
2020-04-01 17:22:55 +0200

    added - ValidationErrorHandler, duplicationErrorHandler

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,6 +1,31 @@
 // ERROR HANDLER MIDDLEWARE - Express automaticaly know if 4 argument added, that is an error handler middleware.
 // Global error handler middleware
 
+const AppError = require('../utils/appError');
+
+// 118. video
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// 119. video
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match('"(.*?)"')[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+// 120. video
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  console.log(errors);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  console.log(message);
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -11,10 +36,23 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // Programming or other unknown error: don't leak error details.
+    // 1) Long error
+    // console.log('ERR', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
@@ -24,7 +62,19 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // TRICK for HARDCOPY -- destructuring { ...variable }.
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    let error = { ...err };
+    /* HA az errorunk CastError, akkor átadjuk a hibát a handleCastErrorDB-nek, amely az AppError-unkal fog visszatérni,
+     * és ez lesz az amit a kliens felé fogunk küldeni.
+     */
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // 11000 is a mongoose douplication error code.
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 
   // console.error('Hello from the error handler');
